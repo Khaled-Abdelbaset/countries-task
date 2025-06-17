4777f62d4d2f0f4d45eec6686fa03b0978e1a6ce
Khaled-Abdelbaset <khaledabdelbaset@gmail.com>
2025-06-18 00:49:08 +0300

    fix: remove unused import statements in HomeView.vue for cleaner code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import AllCountries from '@/components/countries/AllCountries.vue';
+import CountryFilters from '@/components/countries/CountryFilters.vue'
+
 </script>
 
 <template>
-  <main>
-    home
+  <main class="container">
+    <CountryFilters />
+    <AllCountries />
   </main>
 </template>
